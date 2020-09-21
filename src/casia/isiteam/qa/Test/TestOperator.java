37557bd88015e701b1bf077ca5ed603f044a9bf6
Sugarbabyzz <406857586@qq.com>
2020-09-21 17:40:24 +0800

    data update

--- src/casia/isiteam/qa/Test/TestOperator.java
+++ src/casia/isiteam/qa/Test/TestOperator.java
@@ -8,7 +8,7 @@ public class TestOperator {
 
         // 将自定义的 concept 和 entity 别名更新到对应 sameas 别名表中
 //        DbOperator.addConceptAliasToDB();  // concept 2minutes
-//        DbOperator.addEntityAliasToDB(); // entity  10minutes
+//        DbOperator.addEntityAliasToDB(); // entity  6000 entities for 15min
 
         // 将 文件或数据库中原名与别名映射关系 存入数据库的 match_dict 表
 //        DbOperator.getCountryCompareMostToDB();  // 5s
